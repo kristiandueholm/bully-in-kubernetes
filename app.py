ad50e26de02b5345aa069a1566545145222a47d3
Christian Lillelund <chr1000@gmail.com>
2022-12-02 12:00:23 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import asyncio
+from aiohttp import web
+import os
+import socket
+import random
+import aiohttp
+import requests
+
+POD_IP = str(os.environ['POD_IP'])
+WEB_PORT = int(os.environ['WEB_PORT'])
+POD_ID = random.randint(0, 100)
+
+async def setup_k8s():
+    # If you need to do setup of Kubernetes, i.e. if using Kubernetes Python client
+	print("K8S setup completed")
+ 
+async def run_bully():
+    while True:
+        print("Running bully")
+        await asyncio.sleep(5) # wait for everything to be up
+        
+        # Get all pods doing bully
+        ip_list = []
+        print("Making a DNS lookup to service")
+        response = socket.getaddrinfo("bully-service",0,0,0,0)
+        print("Get response from DNS")
+        for result in response:
+            ip_list.append(result[-1][0])
+        ip_list = list(set(ip_list))
+        
+        # Remove own POD ip from the list of pods
+        ip_list.remove(POD_IP)
+        print("Got %d other pod ip's" % (len(ip_list)))
+        
+        # Get ID's of other pods by sending a GET request to them
+        await asyncio.sleep(2)
+        other_pods = dict()
+        for pod_ip in ip_list:
+            endpoint = '/pod_id'
+            url = 'http://' + str(pod_ip) + ':' + str(WEB_PORT) + endpoint
+            response = requests.get(url)
+            other_pods[str(pod_ip)] = response.json()
+            
+        # Other pods in network
+        print(other_pods)
+        
+        # Sleep a bit, then repeat
+        await asyncio.sleep(2)
+    
+#GET /pod_id
+async def pod_id(request):
+    return web.json_response(POD_ID)
+    
+#POST /receive_answer
+async def receive_answer(request):
+    pass
+
+#POST /receive_election
+async def receive_election(request):
+    pass
+
+#POST /receive_coordinator
+async def receive_coordinator(request):
+    pass
+
+async def background_tasks(app):
+    task = asyncio.create_task(run_bully())
+    yield
+    task.cancel()
+    await task
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.router.add_get('/pod_id', pod_id)
+    app.router.add_post('/receive_answer', receive_answer)
+    app.router.add_post('/receive_election', receive_election)
+    app.router.add_post('/receive_coordinator', receive_coordinator)
+    app.cleanup_ctx.append(background_tasks)
+    web.run_app(app, host='0.0.0.0', port=WEB_PORT)
