0a76c28e0d6c25990b14f0ec1238a7adcb8a66a0
kristiandueholm <kristian618@gmail.com>
2023-10-18 14:22:00 +0200

    Co-authored-by: Nestor <TheRealNestor@users.noreply.github.com>

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import random
 import aiohttp
 import requests
 
-POD_IP = str(os.environ['POD_IP'])
-WEB_PORT = int(os.environ['WEB_PORT'])
+# POD_IP = str(os.environ['POD_IP'])
+POD_IP = "127.0.0.1"
+WEB_PORT = 8080
+# WEB_PORT = int(os.environ['WEB_PORT'])
 POD_ID = random.randint(0, 100)
+COORDINATOR = None
 
 async def setup_k8s():
     # If you need to do setup of Kubernetes, i.e. if using Kubernetes Python client
@@ -43,25 +46,57 @@ async def run_bully():
             
         # Other pods in network
         print(other_pods)
+
+        # If leader is not in pool of pods
+            # Elect new leader
+        
+        
         
         # Sleep a bit, then repeat
         await asyncio.sleep(2)
     
+
 #GET /pod_id
 async def pod_id(request):
     return web.json_response(POD_ID)
     
-#POST /receive_answer
-async def receive_answer(request):
+#POST /receive_ok
+async def receive_ok(request):
     pass
 
 #POST /receive_election
+# Request should have id of sender and url where it expects answers. 
 async def receive_election(request):
-    pass
+    data = await request.json()
+    id = data['id']
+    if id < POD_ID:
+
+
+        url = data['url']
+        async with aiohttp.ClientSession() as session:
+            await session.post(url, json={"msg": "OK", "status": 200})
+
+        # Send other election messages
+
+        
+        # start Election
+
+        pass
 
 #POST /receive_coordinator
 async def receive_coordinator(request):
-    pass
+    try:
+        global COORDINATOR
+        data = await request.json()  # Parse the incoming JSON data
+
+        COORDINATOR = data["pod_id"]
+
+        return web.Response(text="OK", status=200)
+
+    except Exception as e:
+        print(f"Error processing coordinator request: {str(e)}")
+        # Return an error response (HTTP status code 500 for internal server error)
+        return web.Response(text="Internal Server Error", status=500)
 
 async def background_tasks(app):
     task = asyncio.create_task(run_bully())
@@ -72,8 +107,8 @@ async def background_tasks(app):
 if __name__ == "__main__":
     app = web.Application()
     app.router.add_get('/pod_id', pod_id)
-    app.router.add_post('/receive_answer', receive_answer)
+    app.router.add_post('/receive_ok', receive_ok)
     app.router.add_post('/receive_election', receive_election)
     app.router.add_post('/receive_coordinator', receive_coordinator)
     app.cleanup_ctx.append(background_tasks)
-    web.run_app(app, host='0.0.0.0', port=WEB_PORT)
+    web.run_app(app, host='127.0.0.1', port=8080)
